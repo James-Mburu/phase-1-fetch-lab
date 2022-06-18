afca4268423575e987db337a7df7b129631723c4
James Wanyoike Mburu <jmburu280@gmail.com>
2022-06-18 13:45:26 +0300

    commit tree 2dec7c114b39c42660863926ca4149ee6c2a3b45

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+
+  let bookPromise = fetch("https://anapioficeandfire.com/api/books")
+
+    // calling the fetch function
+    fetch("https://anapioficeandfire.com/api/books")
+    .then(response => (
+      response.json  
+    )).then(response => (
+      JSON.parse(response)      
+    )).then(response => (
+      renderBooks(response)      
+    ))
+    
+
+    
+
+   
+    // const json = response
+    // const obj = JSON.parse(json)
+   // .then(obj => )
+
+   //renderBooks(obj)
+
+
+    //  bookPromise = fetch("https://anapioficeandfire.com/api/books")
+    // bookPromise
+    // .then((response) => (response.json()))
+    // console.log(response)
+    // 
+
+    // //returning/resolving the fetch function
+    //   .then(resp = resp.json())
+    //       //convert response into json format
+    //   
+
   
 }
 
